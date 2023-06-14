2ac89ccc398134fc32f37d9765c7d9f59eaf30f5
Oleg Markov <o.mark-mail.ru@yandex.ru>
2023-06-14 15:08:47 +0200

    Создание роутов и контроллеров

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,24 @@
 const Card = require('../models/card');
 
-module.exports.createCard = (req, res) => {
-  console.log(req.user._id);
+module.exports.createCard = (req, res, next) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.status(201).send({ data: card }))
+    .catch(next);
 };
+
+module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+  { new: true },
+)
+  .then((likes) => res.send({ data: likes }))
+  .catch(next);
+
+module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $pull: { likes: req.user._id } }, // убрать _id из массива
+  { new: true },
+)
+  .then((likes) => res.send({ data: likes }))
+  .catch(next);
